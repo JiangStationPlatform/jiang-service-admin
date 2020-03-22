b12b5709808c003829a8dd5f617611c414d80aba
jshk123 <jshk123@qq.om>
2020-03-23 00:41:45 +0800

    update

---
+++ src/main/java/cn/jiang/station/platform/service/admin/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package cn.jiang.station.platform.service.admin.mapper;
+
+import cn.jiang.station.platform.service.admin.domain.TbSysUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}
